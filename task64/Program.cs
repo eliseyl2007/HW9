961d6dd49eb0d5c5811e1d2cde1b60d384581266
Elisei <elisey-l@mail.ru>
2023-10-23 19:20:21 +0300

    1

---
+++ task64/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1)
+{
+Console.WriteLine("Ошибка ввода!");
+return;
+}
+
+NaturalNumbers(number);
+
+void NaturalNumbers(int num)
+{
+if(num == 0) return;
+Console.Write($"{num} ");
+NaturalNumbers(num - 1);
+}
